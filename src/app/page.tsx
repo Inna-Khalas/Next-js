fc97ee67c04e2e7e593520ddfb3577e64c3d833b
Inka <innamakarec349@gmail.com>
2025-05-27 14:35:22 +0200

    home-page, dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,11 @@
-// import StatusLabel, { Status } from './components/active-label';
-
-import AddCompanyButton from './components/add-company-button';
+// import AddCompanyButton from './components/add-company-button';
+// import AddPromotionButton from './components/add-promotion-button';
 
 export default function Home() {
-  // console.log(headers());
-
   return (
     <main>
-      {/* <h1>{new Date().toTimeString()}</h1>
-      <StatusLabel status={Status.Active}>Active</StatusLabel>
-      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
-      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel> */}
-      <AddCompanyButton />
+      {/* <AddCompanyButton />
+      <AddPromotionButton /> */}
     </main>
   );
 }
